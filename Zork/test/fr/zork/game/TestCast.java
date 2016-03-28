a5dfd6ca97a5ff14d7cabc54f156137b770eadea
Dahiorus <brondon.ung@gmail.com>
2016-03-29 00:39:01 +0200

    Added combat command "lancer" test.

---
+++ Zork/test/fr/zork/game/TestCast.java
@@ -0,0 +1,23 @@
+package fr.zork.game;
+
+import fr.zork.character.Player;
+import fr.zork.item.Potion;
+import fr.zork.item.Spell;
+
+public class TestCast {
+
+	public static void main(String[] args) {
+		Game game = Game.getInstance();
+		Player player = Player.getInstance();
+		
+		Potion potion = new Potion("potion", 15);
+		Spell spell = new Spell("foudre", 50);
+		
+		player.getBag().add(potion);
+		player.getBag().add(spell);
+		
+		while (!game.displayMenu());
+		game.run();
+	}
+
+}
